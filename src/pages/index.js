4e4f814a3fc3d58c07ea41ba8b091dfa7ed04968
Amanda <amandamarienelson2@gmail.com>
2020-01-18 16:49:02 -0600

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import Button from '../components/Button';
+import Card from '../components/Card';
+import CustomerCard from '../components/CustomerCard';
+import LabelText from '../components/LabelText';
+import Layout from '../components/layout/Layout';
+import SplitSection from '../components/SplitSection';
+import StatsBox from '../components/StatsBox';
+import customerData from '../data/customer-data';
+import HeroImage from '../svg/HeroImage';
+import bryce from '../../public/static/bryce.jpeg';
+import amanda from '../../public/static/amanda.png';
+
+export default () => (
+  <Layout>
+    <section className="pt-20 md:pt-40 h-screen">
+      <div className="container mx-auto px-8 lg:flex">
+        <div className="text-center lg:text-left lg:w-1/2">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+            We help you get online.
+          </h1>
+          <p className="text-xl lg:text-2xl mt-6 font-light">
+            Web design and development, logo design, SEO, social media, content creation, and
+            strategic consulting. <br />
+            <br />
+            <strong>Everything you need to help customers find you.</strong>
+          </p>
+          <p className="mt-8 md:mt-12">
+            <Button size="lg">Contact Us</Button>
+          </p>
+        </div>
+        <div className="lg:w-1/2">
+          <HeroImage />
+        </div>
+      </div>
+    </section>
+    <section id="features" className="py-20 lg:pb-40 lg:pt-48">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl lg:text-5xl font-semibold">Our Services</h2>
+        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Web Design & Development</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Social Media Setup & Integration</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">SEO & Branding</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="features" className="py-20 lg:pb-40">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl lg:text-5xl font-semibold">About</h2>
+        <SplitSection
+          id="services"
+          primarySlot={
+            <div className="lg:pr-32 xl:pr-48">
+              <h3 className="text-3xl font-semibold leading-tight">Amanda Nelson</h3>
+              <p className="mt-8 text-xl font-light leading-relaxed">
+                Fullstack software engineer. Passionate about making it easy for the user.
+              </p>
+            </div>
+          }
+          secondarySlot={
+            <img
+              src={amanda}
+              alt="Amanda Nelson headshot"
+              className="rounded-full h-56 w-56 flex items-center justify-center"
+            />
+          }
+        />
+        <SplitSection
+          reverseOrder
+          primarySlot={
+            <div className="lg:pl-32 xl:pl-48">
+              <h3 className="text-3xl font-semibold leading-tight">Bryce Baker</h3>
+              <p className="mt-8 text-xl font-light leading-relaxed">
+                Digital marketer, content creator, and PR expert.
+              </p>
+            </div>
+          }
+          secondarySlot={
+            <img src={bryce} alt="Bryce Baker headshot" className="rounded-full h-56" />
+          }
+        />
+      </div>
+    </section>
+
+    <section id="stats" className="py-20 lg:pt-32">
+      <div className="container mx-auto text-center">
+        <LabelText className="text-gray-600">Our customers get results</LabelText>
+        <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="testimonials" className="py-20 lg:py-40">
+      <div className="container mx-auto">
+        <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
+        <div className="flex flex-col md:flex-row md:-mx-3">
+          {customerData.map(customer => (
+            <div key={customer.customerName} className="flex-1 px-3">
+              <CustomerCard customer={customer} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+      <p className="mt-8 text-xl font-light">
+        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+      </p>
+      <p className="mt-8">
+        <Button size="xl">Get Started Now</Button>
+      </p>
+    </section>
+  </Layout>
+);
